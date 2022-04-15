a7af231d630a9613cb0a6641c20933dad5bb6bc9
Dario Caric <dario.caric@gmail.com>
2022-04-15 13:59:27 +0200

    mocking test fix

--- src/test/java/com/infybuzz/service/StudentServiceTest.java
+++ src/test/java/com/infybuzz/service/StudentServiceTest.java
@@ -1,20 +1,23 @@
 package com.infybuzz.service;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.infybuzz.entity.Student;
 import com.infybuzz.repository.StudentRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
+
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,11 +25,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
-
-    private MockMvc mockMvc;
-    ObjectMapper objectMapper = new ObjectMapper();
-    ObjectWriter objectWriter = objectMapper.writer();
 
     @InjectMocks
     private StudentService studentService;
@@ -34,15 +34,20 @@ class StudentServiceTest {
     @Mock
     private StudentRepository studentRepository;
 
-
-    Student RECORD_1 = new Student(33L, "dario1", "caric", "dario1.caric@gmail.com");
-    Student RECORD_2 = new Student(34L, "dario2", "caric", "dario2.caric@gmail.com");
-
+    List<Student> records = new ArrayList<>();
 
     @BeforeEach
-    public void setUp(WebApplicationContext context) {
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    public void setUp() {
+        Student RECORD_1 = new Student(33L, "dario1", "caric", "dario1.caric@gmail.com");
+        Student RECORD_2 = new Student(34L, "dario2", "caric", "dario2.caric@gmail.com");
+        records.add(RECORD_1);
+        records.add(RECORD_2);
+
+        //MockitoAnnotations.initMocks(this);
+        //mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        //MockitoAnnotations.openMocks(this);
     }
+
 
     @AfterEach
     void tearDown() {
@@ -55,35 +60,17 @@ class StudentServiceTest {
     @Test
     public void getAllStudents_successful_service() {
 
-        List<Student> records = new ArrayList<>(Arrays.asList(RECORD_1, RECORD_2));
-
+        // when findAll is called from repository
         when(studentRepository.findAll()).thenReturn(records);
 
-        //test start (trigger
-        List<Student> empList = studentService.getAllStudents();
+        // start test and save result in studentList
+        List<Student> studentList = studentService.getAllStudents();
 
         // test verification, some attributes etc.
-        assertEquals(2, empList.size());
-        assertEquals("dario2", empList.get(1).getFirstName());
-
+        assertEquals(2, studentList.size());
+        assertEquals("dario2", studentList.get(1).getFirstName());
         verify(studentRepository, times(1)).findAll();
     }
 
-    @Test
-    void getById_successful() throws Exception {
 
-
-    }
-
-    @Test
-    void createStudent() {
-    }
-
-    @Test
-    void updateStudent() {
-    }
-
-    @Test
-    void deleteStudent() {
-    }
 }
